7de39d701070046326e247e31073773517367c2b
katieolson84 <katieolson84@gmail.com>
2021-03-12 12:24:18 -0700

    post projects is working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,30 @@
 // Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model');
+const { validateAction } = require('../middleware/middleware')
+
+const router = express.Router();
+
+// Get Action 
+router.get('/:id/actions', validateAction, (req,res,next)=> {
+    Action.find(req.query)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(next);
+    // .catch(err => {
+    //     res.status(500).json({
+    //         message: "The action information could not be retrieved", err
+    //     })
+    // })
+})
+
+// Get Action by Id
+
+// Post Action
+
+// Update PUT Action
+
+// Delete Action
+
+module.exports = router;
